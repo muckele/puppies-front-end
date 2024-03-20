de93b2a5107de488c66b7d9c64eabd28760a634a
Mathew <muckele7@gmail.com>
2024-03-19 21:42:46 -0400

    creates navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//npm modules
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Landing from './pages/Landing/Landing'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Landing />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+//NPM modules
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+//app
+import App from './App.jsx'
+
+//CSS
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
