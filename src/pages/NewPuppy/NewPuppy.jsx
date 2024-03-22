6384ace9640de1f852998c73630fd57f845f009b
Mathew <muckele7@gmail.com>
2024-03-21 22:49:41 -0400

    finshes create puppy functionality

--- src/pages/NewPuppy/NewPuppy.jsx
+++ src/pages/NewPuppy/NewPuppy.jsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 import './NewPuppy.css'
 
 const NewPuppy = (props) => {
-    const [form, setForm] = useState({
+  const [form, setForm] = useState({
     name: '',
     breed: '',
     age: 0,
@@ -13,10 +13,19 @@ const NewPuppy = (props) => {
     ears: 'Button'
   })
 
+  const handleChange = (evt) => {
+    setForm({ ...form, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleAddPuppy(form)
+  }
+
   return (
     <main className="new">
       <h1>NEW PUPPY</h1>
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={handleSubmit}>
         <label htmlFor="name-input">Name</label>
         <input
           required
@@ -24,8 +33,11 @@ const NewPuppy = (props) => {
           name="name"
           id="name-input"
           placeholder="Name"
+          value={form.name}
+          onChange={handleChange}
         />
-        <label htmlFor="age-input">Age</label>
+
+        <label htmlFor="title-input">Age</label>
         <input
           required
           min="0"
@@ -33,6 +45,8 @@ const NewPuppy = (props) => {
           name="age"
           id="age-input"
           placeholder="Age"
+          value={form.age}
+          onChange={handleChange}
         />
 
         <label htmlFor="breed-input">Breed</label>
@@ -42,10 +56,18 @@ const NewPuppy = (props) => {
           name="breed"
           id="breed-input"
           placeholder="Breed"
+          value={form.breed}
+          onChange={handleChange}
         />
 
         <label htmlFor="color-input">Color</label>
-        <select required name="color" id="color-input">
+        <select 
+          required 
+          name="color" 
+          id="color-input"
+          value={form.color}
+          onChange={handleChange}
+        >
           <option value="Grey">Grey</option>
           <option value="Black">Black</option>
           <option value="Brown">Brown</option>
@@ -54,7 +76,13 @@ const NewPuppy = (props) => {
         </select>
 
         <label htmlFor="ears-input">Ears</label>
-        <select required name="ears" id="ears-input">
+        <select 
+          required 
+          name="ears" 
+          id="ears-input"
+          value={form.ears}
+          onChange={handleChange}
+        >
           <option value="Drop">Drop</option>
           <option value="Pointy">Pointy</option>
           <option value="Button">Button</option>
